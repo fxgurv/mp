be8db15268f8bb39eb404bc1e29fc1a527e93065
Strange <168190107+fxgurv@users.noreply.github.com>
2024-10-18 16:02:51 -0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,12 +8,6 @@ from termcolor import colored
 ROOT_DIR = os.path.dirname(sys.path[0])
 
 def assert_folder_structure() -> None:
-    """
-    Make sure that the nessecary folder structure is present.
-
-    Returns:
-        None
-    """
     # Create the .mp folder
     if not os.path.exists(os.path.join(ROOT_DIR, ".mp")):
         if get_verbose():
@@ -21,171 +15,69 @@ def assert_folder_structure() -> None:
         os.makedirs(os.path.join(ROOT_DIR, ".mp"))
 
 def get_first_time_running() -> bool:
-    """
-    Checks if the program is running for the first time by checking if .mp folder exists.
-
-    Returns:
-        exists (bool): True if the program is running for the first time, False otherwise
-    """
     return not os.path.exists(os.path.join(ROOT_DIR, ".mp"))
 
 def get_email_credentials() -> dict:
-    """
-    Gets the email credentials from the config file.
-
-    Returns:
-        credentials (dict): The email credentials
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["email"]
 
 def get_verbose() -> bool:
-    """
-    Gets the verbose flag from the config file.
-
-    Returns:
-        verbose (bool): The verbose flag
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["verbose"]
 
 def get_browser_profile_path() -> str:
-    """
-    Gets the path to the Firefox profile.
-
-    Returns:
-        path (str): The path to the Firefox profile
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["profile_path"]
 
 def get_headless() -> bool:
-    """
-    Gets the headless flag from the config file.
-
-    Returns:
-        headless (bool): The headless flag
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["headless"]
 
 def get_model() -> str:
-    """
-    Gets the model from the config file.
-
-    Returns:
-        model (str): The model
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["llm"]
 
-def get_twitter_language() -> str:
-    """
-    Gets the Twitter language from the config file.
-
-    Returns:
-        language (str): The Twitter language
-    """
+def get_language() -> str:
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
-        return json.load(file)["twitter_language"]
+        return json.load(file)["language"]
 
 def get_image_model() -> str:
-    """
-    Gets the Image MOdel from the config file.
-
-    Returns:
-        model (str): The image model
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["image_model"]
 
 def get_threads() -> int:
-    """
-    Gets the amount of threads to use for example when writing to a file with MoviePy.
-
-    Returns:
-        threads (int): Amount of threads
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["threads"]
     
 def get_image_prompt_llm() -> str:
-    """
-    Gets the image prompt for LLM from the config file.
-
-    Returns:
-        prompt (str): The image prompt
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["image_prompt_llm"]
 
 def get_zip_url() -> str:
-    """
-    Gets the URL to the zip file containing the songs.
-
-    Returns:
-        url (str): The URL to the zip file
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["zip_url"]
 
 def get_is_for_kids() -> bool:
-    """
-    Gets the is for kids flag from the config file.
-
-    Returns:
-        is_for_kids (bool): The is for kids flag
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["is_for_kids"]
 
 def get_google_maps_scraper_zip_url() -> str:
-    """
-    Gets the URL to the zip file containing the Google Maps scraper.
-
-    Returns:
-        url (str): The URL to the zip file
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["google_maps_scraper"]
 
 def get_google_maps_scraper_niche() -> str:
-    """
-    Gets the niche for the Google Maps scraper.
-
-    Returns:
-        niche (str): The niche
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["google_maps_scraper_niche"]
 
 def get_scraper_timeout() -> int:
-    """
-    Gets the timeout for the scraper.
-
-    Returns:
-        timeout (int): The timeout
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["scraper_timeout"] or 300
 
 def get_outreach_message_subject() -> str:
-    """
-    Gets the outreach message subject.
-
-    Returns:
-        subject (str): The outreach message subject
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["outreach_message_subject"]
     
 def get_outreach_message_body_file() -> str:
-    """
-    Gets the outreach message body file.
-
-    Returns:
-        file (str): The outreach message body file
-    """
     with open(os.path.join(ROOT_DIR, "config.json"), "r") as file:
         return json.load(file)["outreach_message_body_file"]
 
